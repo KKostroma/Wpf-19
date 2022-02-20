37d5f50f4b30aed9590079293490ac19b1e4c71c
Ekaterina Kostromitskaia <kostroma.mi@mail.ru>
2022-02-20 16:12:17 +0300

    Финализация

---
+++ WpfЗадание19/Models/Ariph.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfЗадание19.Models
+{
+    static class Ariph
+    {
+        public static int Add(int a)
+        {
+            return (int)(2 * Math.PI * a);
+        }
+    }
+}
